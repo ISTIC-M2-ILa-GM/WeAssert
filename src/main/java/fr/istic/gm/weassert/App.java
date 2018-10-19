70c48eb53e159c4975240e32df081af079b989b6
Malah <malah@neuf.fr>
2018-10-19 11:01:22 +0200

    initialized project

---
+++ src/main/java/fr/istic/gm/weassert/App.java
@@ -0,0 +1,8 @@
+package fr.istic.gm.weassert;
+
+public class App {
+
+    public static void main(String... args) {
+
+    }
+}
---
+++ src/test/java/fr/istic/gm/weassert/AppTest.java
@@ -0,0 +1,11 @@
+package fr.istic.gm.weassert;
+
+import org.junit.Test;
+
+public class AppTest {
+
+    @Test
+    public void shouldStartTheApplication() {
+        App.main();
+    }
+}
