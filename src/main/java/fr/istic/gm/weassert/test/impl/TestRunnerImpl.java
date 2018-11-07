75690358e53ca7bdd0f6f3db2d23659d33e687e3
Malah <malah@neuf.fr>
2018-11-07 09:44:38 +0100

    worked on test run listener

--- src/main/java/fr/istic/gm/weassert/test/impl/TestRunnerImpl.java
+++ src/main/java/fr/istic/gm/weassert/test/impl/TestRunnerImpl.java
@@ -1,11 +1,10 @@
 package fr.istic.gm.weassert.test.impl;
 
+import fr.istic.gm.weassert.test.TestRunListener;
 import fr.istic.gm.weassert.test.TestRunner;
 import lombok.extern.java.Log;
 import org.junit.runner.JUnitCore;
-import org.junit.runner.Result;
 
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLClassLoader;
 import java.util.List;
@@ -27,11 +26,8 @@ public class TestRunnerImpl implements TestRunner {
         try {
             Class<?> clazz = urlClassLoader.loadClass(className);
             JUnitCore core = new JUnitCore();
-            Result result = core.run(clazz);
-            log.info("FINISHED");
-            log.info(String.format("| IGNORED: %d", result.getIgnoreCount()));
-            log.info(String.format("| FAILURES: %d", result.getFailureCount()));
-            log.info(String.format("| RUN: %d", result.getRunCount()));
+            core.addListener(new TestRunListener());
+            core.run(clazz);
         } catch (ClassNotFoundException e) {
             String message = String.format(LOAD_CLASS_ERROR, e.getCause());
             log.info(message);
@@ -41,12 +37,6 @@ public class TestRunnerImpl implements TestRunner {
 
 
     private URL mapToUrl(String a) {
-        try {
-            return new URL(a);
-        } catch (MalformedURLException e) {
-            String message = String.format(PARSED_ERROR, e.getCause());
-            log.info(message);
-            throw new RuntimeException(message, e);
-        }
+        return getClass().getResource(a);
     }
 }
