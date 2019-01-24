29f69b1d6061370f9d2af36f44226d11c28a6429
Gautier Rouleau <gautier.rouleau@gmail.com>
2019-01-24 09:57:41 +0100

    added test for CodeWriterImpl class

--- src/main/java/fr/istic/gm/weassert/test/CodeWriter.java
+++ src/main/java/fr/istic/gm/weassert/test/CodeWriter.java
@@ -3,6 +3,5 @@ package fr.istic.gm.weassert.test;
 import java.util.List;
 
 public interface CodeWriter {
-    void insertOne(String methodName, String code);
     void insertMany(String methodName, List<String> codes);
 }
--- src/test/java/fr/istic/gm/weassert/test/CodeWriterTest.java
+++ src/test/java/fr/istic/gm/weassert/test/CodeWriterTest.java
@@ -1,5 +1,38 @@
-import static org.junit.Assert.*;
+package fr.istic.gm.weassert.test;
+
+import fr.istic.gm.weassert.test.impl.CodeWriterImpl;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class CodeWriterTest {
+    private CodeWriterImpl codeWriter;
 
+    @Before
+    public void setUp() {
+        this.codeWriter = new CodeWriterImpl(getClass());
+
+        assertEquals(this.codeWriter.getClassName(), getClass().getName());
+    }
+
+    @Test
+    public void insertMany() {
+        List<String> sourceCode = Arrays.asList(
+                "System.out.println(\"Loco loco\");",
+                "System.out.println(\"Loca loca\");"
+        );
+
+        this.codeWriter.insertMany("truc", sourceCode);
+
+        assertTrue(this.codeWriter.getClassContainer().isFrozen());
+    }
+
+    public void truc() {
+
+    }
 }
