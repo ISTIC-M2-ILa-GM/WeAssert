4883cc7116cceed89a71623be44668607ae8c3ce
Gautier Rouleau <gautier.rouleau@gmail.com>
2019-01-30 14:13:45 +0100

    added Basic UI with buttons and text area, added file chooser window, #9

--- src/main/java/fr/istic/gm/weassert/controller/MainController.java
+++ src/main/java/fr/istic/gm/weassert/controller/MainController.java
@@ -1,4 +1,31 @@
 package fr.istic.gm.weassert.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.stage.DirectoryChooser;
+import javafx.stage.FileChooser;
+
+import java.io.File;
+
 public class MainController {
+    private File file;
+
+    @FXML
+    public Label selectedFile;
+
+    @FXML
+    public Button browseButton;
+
+
+    public void initialize() {
+
+    }
+
+    public void browseAction(ActionEvent actionEvent) {
+        DirectoryChooser directoryChooser = new DirectoryChooser();
+        this.file = directoryChooser.showDialog(null);
+        this.selectedFile.setText(this.file.getPath());
+    }
 }
