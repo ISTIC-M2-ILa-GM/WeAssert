6fb8803986a4447d7b1a356196c545a6dd7332d8
Gautier Rouleau <gautier.rouleau@gmail.com>
2019-02-01 08:58:10 +0100

    renamed CodeWriterImpl to JavassistCodeWriter, created new impl #12

---
+++ src/main/java/fr/istic/gm/weassert/test/impl/SourceCodeWriter.java
@@ -0,0 +1,57 @@
+package fr.istic.gm.weassert.test.impl;
+
+import fr.istic.gm.weassert.test.CodeWriter;
+import fr.istic.gm.weassert.test.exception.WeAssertException;
+import javassist.*;
+import lombok.Getter;
+
+import java.util.List;
+
+@Getter
+public class SourceCodeWriter implements CodeWriter {
+    private String className;
+
+    private CtClass classContainer;
+
+    public SourceCodeWriter(Class clazz) {
+        ClassPool pool = ClassPool.getDefault();
+
+        try {
+            this.classContainer = pool.get(clazz.getName());
+            this.className = clazz.getName();
+        } catch (NotFoundException e) {
+            throw new WeAssertException(
+                    String.format("CodeWriter: could not find class named %s", clazz.getName()), e
+            );
+        }
+    }
+
+    public void insertOne(String methodName, String desc, String code) {
+        try {
+            CtMethod method = this.classContainer.getMethod(methodName, desc);
+            try {
+                method.insertAfter(code);
+            } catch (CannotCompileException e) {
+                throw new WeAssertException("JavassitCodeWriter: could not insert code", e);
+            }
+        } catch (NotFoundException e) {
+            throw new WeAssertException(
+                    String.format("JavassitCodeWriter: could not find method named %s", methodName), e
+            );
+        }
+    }
+
+    @Override
+    public void insertMany(String methodName, String desc, List<String> codes) {
+        codes.forEach(src -> this.insertOne(methodName, desc, src));
+    }
+
+    @Override
+    public void writeAndCloseFile() {
+        try {
+            this.classContainer.writeFile();
+        } catch (Exception e) {
+            throw new WeAssertException("JavassitCodeWriter: could not write file", e);
+        }
+    }
+}
