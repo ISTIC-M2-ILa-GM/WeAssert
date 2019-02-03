576346730e16a8c54ee7e15c28d18475265733d8
malahx <gwenolelh@aol.com>
2019-02-03 14:58:04 +0100

    enhanced local variable parser to add getters

--- src/main/java/fr/istic/gm/weassert/test/analyser/impl/LocalVariableParserImpl.java
+++ src/main/java/fr/istic/gm/weassert/test/analyser/impl/LocalVariableParserImpl.java
@@ -1,19 +1,31 @@
 package fr.istic.gm.weassert.test.analyser.impl;
 
 import fr.istic.gm.weassert.test.analyser.LocalVariableParser;
+import fr.istic.gm.weassert.test.exception.WeAssertException;
 import fr.istic.gm.weassert.test.model.LocalVariableParsed;
 import fr.istic.gm.weassert.test.utils.ClassReaderFactory;
 import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
 import org.objectweb.asm.ClassReader;
+import org.objectweb.asm.Type;
 import org.objectweb.asm.tree.ClassNode;
+import org.objectweb.asm.tree.LocalVariableNode;
 import org.objectweb.asm.tree.MethodNode;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
+import java.util.Objects;
 import java.util.stream.Collectors;
+
+import static java.util.Arrays.asList;
 
 @Slf4j
 public class LocalVariableParserImpl implements LocalVariableParser {
+
+    private static final List<Integer> PRIMITIVE_TYPE = asList(Type.BOOLEAN, Type.DOUBLE, Type.FLOAT, Type.INT, Type.LONG, Type.CHAR, Type.SHORT);
 
     private ClassReader classReader;
 
@@ -32,7 +44,14 @@ public class LocalVariableParserImpl implements LocalVariableParser {
 
         log.info("LOCAL VARIABLE PARSE...");
         classReader.accept(classNode, 0);
-        List<LocalVariableParsed> variableParseds = classNode.methods.stream()
+        List<LocalVariableParsed> variableParseds = mapMethodsToLocalVariableParsed(classNode.methods);
+        log.info("LOCAL VARIABLE PARSED: " + variableParseds);
+
+        return variableParseds;
+    }
+
+    private List<LocalVariableParsed> mapMethodsToLocalVariableParsed(List<MethodNode> methods) {
+        return methods.stream()
                 .filter(m -> !"<init>".equals(m.name))
                 .map(m -> LocalVariableParsed.builder()
                         .name(m.name)
@@ -40,14 +59,53 @@ public class LocalVariableParserImpl implements LocalVariableParser {
                         .localVariables(retrieveLocalVariables(m))
                         .build())
                 .collect(Collectors.toList());
-        log.info("LOCAL VARIABLE PARSED: " + variableParseds);
-
-        return variableParseds;
     }
 
     private List<String> retrieveLocalVariables(MethodNode methodNode) {
         return methodNode.localVariables.stream()
                 .filter(v -> !"this".equals(v.name))
-                .map(v -> v.name).collect(Collectors.toList());
+                .map(this::retrieveVariableName)
+                .filter(Objects::nonNull)
+                .flatMap(Collection::stream)
+                .collect(Collectors.toList());
+    }
+
+    private List<String> retrieveVariableName(LocalVariableNode localVariable) {
+        List<String> variables = new ArrayList<>();
+        Type t = Type.getType(localVariable.desc);
+        if (t.getSort() == Type.OBJECT) {
+            variables.addAll(retrieveGetters(localVariable, t));
+        } else if (PRIMITIVE_TYPE.contains(t.getSort())) {
+            variables.add(localVariable.name);
+        }
+        return variables;
+    }
+
+    private List<String> retrieveGetters(LocalVariableNode localVariable, Type type) {
+        List<String> variables = new ArrayList<>();
+        try {
+            Class<?> c = Class.forName(type.getClassName());
+            for (Field f : c.getDeclaredFields()) {
+                if (f.getName() == null || f.getName().isEmpty()) {
+                    continue;
+                }
+                String methodName = String.format("get%s%s", f.getName().substring(0, 1).toUpperCase(), f.getName().substring(1));
+                if (contains(c.getDeclaredMethods(), methodName)) {
+                    variables.add(String.format("%s.%s()", localVariable.name, methodName));
+                }
+            }
+        } catch (ClassNotFoundException e) {
+            throw new WeAssertException("Can't found a variable type", e);
+        }
+        return variables;
+    }
+
+    private boolean contains(Method[] declaredMethods, String methodName) {
+        for (Method m : declaredMethods) {
+            if (methodName.equals(m.getName())) {
+                return true;
+            }
+        }
+        return false;
     }
 }
