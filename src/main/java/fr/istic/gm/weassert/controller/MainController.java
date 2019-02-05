1b546ff34dad6eab314fff0194047ed594840698
Gautier Rouleau <gautier.rouleau@gmail.com>
2019-02-05 18:30:03 +0100

    added file TreeView #9

--- src/main/java/fr/istic/gm/weassert/controller/MainController.java
+++ src/main/java/fr/istic/gm/weassert/controller/MainController.java
@@ -1,26 +1,32 @@
 package fr.istic.gm.weassert.controller;
 
 import com.sun.javafx.collections.ObservableListWrapper;
+import fr.istic.gm.weassert.test.utils.FileUtils;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.chart.PieChart;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextArea;
+import javafx.scene.control.*;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.web.WebEngine;
+import javafx.scene.web.WebView;
 import javafx.stage.DirectoryChooser;
+import javafx.stage.FileChooser;
+import sun.reflect.generics.tree.Tree;
 
 import java.io.File;
 import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import static javafx.scene.chart.PieChart.Data;
 
 public class MainController {
-    private File file;
-
-
     @FXML
     public PieChart testResultsChart;
+
+    @FXML
+    public Label selectedMaven;
 
     @FXML
     public Label selectedFile;
@@ -29,14 +35,28 @@ public class MainController {
     public Button browseButton;
 
     @FXML
-    public TextArea logTextArea;
+    public WebView webView;
+
+    @FXML
+    public TreeView treeView;
+
+    private File file;
+
+    private File mavenBinary;
 
     private Data passedTest;
 
     private Data failedTests;
 
+    private WebEngine webEngine;
+
 
     public void initialize() {
+        this.webEngine = this.webView.getEngine();
+        TreeItem<String> rootItem = new TreeItem<>("[No project selected]");
+        rootItem.setExpanded(true);
+        treeView.setRoot(rootItem);
+
         this.testResultsChart.setStartAngle(45);
 
         this.passedTest = new Data("Passed tests", 1);
@@ -53,22 +73,52 @@ public class MainController {
             this.selectedFile.setText("[No project selected]");
         } else {
             this.selectedFile.setText(this.file.getPath());
+            this.treeView.getRoot().setValue(this.file.getName());
+            this.treeView.getRoot().getChildren().clear();
+            List<TreeItem<String>> collect = FileUtils.findFilesFromFolder(file)
+                    .stream().filter(f -> f.getName().endsWith("Test.java"))
+                    .map(f -> new TreeItem<String>(f.getName()))
+                    .collect(Collectors.toList());
+
+            this.treeView.getRoot().getChildren().addAll(collect);
         }
-        this.appendLogToTextArea("MainController.browseAction: " + selectedFile.getText());
     }
 
-    public void appendLogToTextArea(String s) {
-        this.logTextArea.appendText(s + "\n");
+    public void displaySourceCodeWithHiglights(String s) {
+        this.webEngine.loadContent(String.format("<html><body><pre><code>" +
+                "%s" +
+                "</code></pre></body>", s));
     }
 
     public void generateAction(ActionEvent actionEvent) {
-        this.appendLogToTextArea("MainController.generateAction: started assertions generation...");
         // TODO: start assertions generation
     }
 
     public void testAction(ActionEvent actionEvent) {
-        this.appendLogToTextArea("MainController.testAction: started tests...");
         // TODO: start tests
         this.passedTest.setPieValue(8);
+    }
+
+    public void selectMavenAction(ActionEvent actionEvent) {
+        FileChooser fileChooser = new FileChooser();
+        this.mavenBinary = fileChooser.showOpenDialog(null);
+        if (this.mavenBinary == null) {
+            this.selectedMaven.setText("[System default]");
+        } else {
+            this.selectedMaven.setText(this.file.getPath());
+        }
+    }
+
+    public void addClassToTreeView(String className) {
+        TreeItem root = this.treeView.getRoot();
+        TreeItem<String> item = new TreeItem<> (className);
+        root.getChildren().add(item);
+    }
+
+    public void itemClickedAction(MouseEvent mouseEvent) {
+        MultipleSelectionModel selectionModel = this.treeView.getSelectionModel();
+        ObservableList<TreeItem> selectedItems = selectionModel.getSelectedItems();
+        // TODO: show corresponding class in WebView
+        // System.out.println(selectedItems.get(0).getValue());
     }
 }
